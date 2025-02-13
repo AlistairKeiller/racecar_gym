8b445a8c7eb3f4eee1f5113344ae26e4d72331e3
Alistair Keiller <alistair@keiller.net>
2025-02-12 19:17:08 -0800

    update run.py to just use Austria by default

--- run.py
+++ run.py
@@ -1,18 +1,9 @@
 import gymnasium
 import racecar_gym.envs.gym_api
 
-# For predefined environments:
 env = gymnasium.make(
     id='SingleAgentAustria-v0',
     render_mode='human'
-)
-
-# For custom scenarios:
-env = gymnasium.make(
-    id='SingleAgentRaceEnv-v0', 
-    scenario='examples/scenarios/austria.yml',
-    render_mode='rgb_array_follow', # optional
-    render_options=dict(width=320, height=240, agent='A') # optional
 )
 
 done = False
@@ -20,7 +11,6 @@ reset_options = dict(mode='grid')
 obs, info = env.reset(options=reset_options)
 
 while not done:
-    print("test")
     action = env.action_space.sample()
     obs, rewards, terminated, truncated, states = env.step(action)
     done = terminated or truncated
